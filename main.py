f6a59350711d7523599a7f4817e432d285eac054
flamtz <friedrichmatz2002@gmail.com>
2023-02-02 10:38:25 +0100

    Passwort generator

---
+++ main.py
@@ -0,0 +1,19 @@
+# imports
+import random
+
+# passwort länge
+passwort_laenge = 10
+
+# variablen
+i = 0
+passwort = ""
+
+# wenn i (bei jeder ascii zahl in zulässiger range incremented) unter passwort länge --> nochmal, bis länge erreicht
+while i < passwort_laenge:
+    asciiint = random.randint(0, 127)
+    if asciiint in range(33, 63) or asciiint in range(63, 89) or asciiint in range(93, 122):
+        i += 1
+        passwort = passwort + chr(asciiint)
+
+# ausgabe
+print("Das Passwort lautet: " + passwort)
