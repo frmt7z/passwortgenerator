2335541b6ff27944d11813abb8c36547599bacd4
flamtz <friedrichmatz2002@gmail.com>
2023-02-02 11:09:22 +0100

    Passwort generator

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ passwort = ""
 # wenn i (bei jeder ascii zahl in zulässiger range incremented) unter passwort länge --> nochmal, bis länge erreicht
 while i < passwort_laenge:
     asciiint = random.randint(0, 127)
-    if asciiint in range(33, 63) or asciiint in range(63, 89) or asciiint in range(93, 122):
+    if asciiint in range(33, 63) or asciiint in range(65, 89) or asciiint in range(93, 122):
         i += 1
         passwort = passwort + chr(asciiint)
 
